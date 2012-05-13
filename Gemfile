19daf99631c4e6bf6e739b2c29c81293d814545e
Tyler Gannon <tgannon@gmail.com>
2012-05-13 14:05:02 -0700

    Added devise plus views and members model

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'pg'
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'devise'
+gem 'omniauth-facebook'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'vcr'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudFunded::Application.config.secret_token = '7132330b03143cef3f008c77d44d5ffc0228916306092a9fe33f649bed2e0f41e15a33f5f1dbdfd24f9d0cb0a6ce70769137201c1589e37c9fa809d82c976bb0'
