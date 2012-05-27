cffbb1d7607ec6eb8b4f53a32a466e1c2a8e88d8
Tyler Gannon <tgannon@gmail.com>
2012-05-27 08:51:54 -0700

    should authorize in the controls controller.

--- app/controllers/admin/controls_controller.rb
+++ app/controllers/admin/controls_controller.rb
@@ -1,6 +1,10 @@
 class Admin::ControlsController < ApplicationController
+  before_filter :authenticate_admin
   def index
     
   end
   
+  def authenticate_admin
+    authorize! :manage, Page
+  end
 end
