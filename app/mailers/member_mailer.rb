077c241ffeedc0537df5fd76410f38ba6eac7d6f
Tyler Gannon <tgannon@gmail.com>
2012-07-03 16:41:38 -0700

    oopsie

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,7 +1,7 @@
 class MemberMailer < ActionMailer::Base
   default from: "welcome-no-reply@cloudfunded.com"
   
-  def new_project(member)
+  def new_member(member)
     @member = member
     mail(to: member.email, subject: "CloudFunded: New Project Added", from: from_address)
   end
