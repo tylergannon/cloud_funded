221b7ce99c8ccfb6fc14aeedf92f792088ed4128
Tyler Gannon <tgannon@gmail.com>
2012-07-25 23:00:34 -0700

    Completed Stripe configuration.

--- spec/requests/project_pledge_spec.rb
+++ spec/requests/project_pledge_spec.rb
@@ -7,7 +7,7 @@ feature "Pledging For A Project" do
   end
   
   describe "when I don't have a dwolla account" do
-    scenario "I should be able to submit my pledge and then sign up for dwolla." do
+    scenario "I should be able to submit my pledge and then pay." do
       visit project_path(@project)
       click_link 'new_pledge'
       current_path.should == new_project_pledge_path(@project)
@@ -36,6 +36,26 @@ feature "Pledging For A Project" do
       page.should have_content 'Thanks!'
       @pledge.reload
       @pledge.should be_payment_received
+    end
+    
+    scenario "Paying with a declined credit card", js: true do
+      VCR.use_cassette "features/project_pledge_spec/Paying with a declined credit card" do
+        @pledge = FactoryGirl.create :pledge_pay_by_cc, investor: @member, project: @project, perk: @project.perks[0]
+        visit new_project_pledge_path(@project, 'pay_by_cc')
+        within '.pay' do
+          fill_in 'cc_number', with: '4000000000000002'
+          fill_in 'cc_cvc', with: '123'
+          fill_in 'cc_month', with: '12'
+          fill_in 'cc_year', with: (Date.today.year+1)
+          click_button 'Pay'
+        end
+        wait_until(5) do
+          page.has_selector?('.payment_error')
+        end
+        within '.payment_error' do
+          page.should have_content("Your card was declined")
+        end
+      end
     end
   end
 end
