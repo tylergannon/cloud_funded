864af7be9b804579519f3806f874654855b198ef
Tyler Gannon <tgannon@gmail.com>
2012-07-30 16:15:16 -0700

    Checking in before merging recent develop changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require jquery-ui-1.8.21.custom.min
 //= require 3rd_party/jquery.html5_upload.js
 //= require jquery.easing.1.3
