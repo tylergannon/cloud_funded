1e90787752d8cf1ad7d72f248c12fbc181266390
Tyler Gannon <tgannon@gmail.com>
2012-08-02 08:10:41 -0700

    accidentally 'uncommented' a comment resulting in syntax error.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,7 +85,7 @@ CloudFunded::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  Do not compress assets
+  # Do not compress assets
   config.assets.compress = false
   
   # Expands the lines which load the assets
