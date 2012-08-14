89c24a957d2605d9da34faffeb705e15274b95ed
Tyler Gannon <tgannon@gmail.com>
2012-08-14 00:25:34 -0700

    update cap deploy code

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,9 @@ set :scm, :git
 
 default_run_options[:pty] = true
 
-
 after "deploy", "deploy:migrate"
 after "deploy:migrate", "deploy:restart"
-after "deploy:restart", "deploy:cleanup"
+#  after "deploy:restart", "deploy:cleanup"
 
 require 'bundler/capistrano'
 require "rvm/capistrano"
@@ -25,3 +24,15 @@ require "rvm/capistrano"
 task :restart, :roles => [:app], :except => {:no_release => true} do
   run "cd #{deploy_to}/current && touch tmp/restart.txt"
 end
+
+namespace :deploy do
+  task :migrate, :roles => :web, :except => { :no_release => true } do
+    from = source.next_revision(current_revision)
+    if capture("cd #{latest_release} && #{source.local.log(from)} db/migrate/ | wc -l").to_i > 0
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} db:migrate}
+    else
+      logger.info "Skipping migration because there were no migration changes"
+    end
+  end
+end
+
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,16 @@ set :deploy_to, '/home/tyler/src/prod/cloud_funded'
 set :branch, 'master'
 set :rails_env, 'production'
 load 'deploy/assets'
+
+# namespace :deploy do
+#   namespace :assets do
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#     end
+#   end
+# end
